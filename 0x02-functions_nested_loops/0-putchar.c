a4b259bdc20f1cd4d4cf0e4d684509465b809dd3
natnael <natnaelshiferaw2123@gmail.com>
2023-07-18 13:20:52 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+/**
+* main - a program that prints _putchar
+* Return : 0 (success)
+*/
+int main(void)
+{
+	system("echo \'_putchar\'");
+	return (0);
+}
