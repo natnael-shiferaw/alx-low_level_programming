6a770f9878db1b0ad3f927c21e8a9918f99de72b
natnael <natnaelshiferaw2123@gmail.com>
2023-07-18 15:33:53 +0000

    checks for alphabetic order

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isalpha - checks if the input is lowercase or uppercase
+* @c: parameter to be checked out
+* Return: 1 if right, 0 otherwise
+*/
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
