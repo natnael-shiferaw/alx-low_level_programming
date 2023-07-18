bc5fa2df0b1c7a53adbbf11ae4fa54a87c847360
natnael <natnaelshiferaw2123@gmail.com>
2023-07-18 14:03:28 +0000

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
+#include "main.h"
 /**
-* main - a program that prints _putchar
-* Return : 0 (success)
+* main - print _putchar
+*
+* Return: 0
 */
+
 int main(void)
 {
-	system("echo \'_putchar\'");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
