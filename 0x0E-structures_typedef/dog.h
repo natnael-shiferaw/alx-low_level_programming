4ce41955d3953de38b7552f897e3691849e1fbd1
natnael <natnaelshiferaw2123@gmail.com>
2023-08-14 22:36:56 +0000

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,12 @@
 #define DOG_H
 
 /**
- *  * struct dog - a dog's basic info
- *   * @name: First member
- *    * @age: Second member
- *     * @owner: Third member
- *      *
- *       * Description: Longer description
- *        */
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+* Description: Longer description
+*/
 struct dog
 {
 	char *name;
@@ -17,8 +16,8 @@ struct dog
 };
 
 /**
- *  * dog_t - typedef for struct dog
- *   */
+* dog_t - typedef for struct dog
+*/
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
