6a9de136f6d0182f91d0a5f108e5274fe4b10318
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2023-09-04 20:03:45 +0000

    updated

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -23,19 +23,19 @@ void check_elf(unsigned char *e_ident);
 */
 void check_elf(unsigned char *e_ident)
 {
-	int INDEX;
+int INDEX;
 
-	for (INDEX = 0; index < 4; INDEX++)
-	{
-		if (e_ident[INDEX] != 127 &&
-				    e_ident[INDEX] != 'E' &&
-				        e_ident[INDEX] != 'L' &&
-					    e_ident[INDEX] != 'F')
-		{
-			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-			exit(98);
-		}
-	}
+for (INDEX = 0; INDEX < 4; INDEX++)
+{
+if (e_ident[INDEX] != 127 &&
+e_ident[INDEX] != 'E' &&
+e_ident[INDEX] != 'L' &&
+e_ident[INDEX] != 'F')
+{
+dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+exit(98);
+}
+}
 }
 
 /**
@@ -44,19 +44,19 @@ void check_elf(unsigned char *e_ident)
 */
 void print_magic(unsigned char *e_ident)
 {
-	int INDEX;
+int INDEX;
 
-	printf(" Magic: ");
+printf(" Magic: ");
 
-	for (INDEX = 0; INDEX < EI_NIDENT; INDEX++)
-	{
-		printf("%02x", e_ident[INDEX]);
+for (INDEX = 0; INDEX < EI_NIDENT; INDEX++)
+{
+printf("%02x", e_ident[INDEX]);
 
-		if (INDEX == EI_NIDENT - 1)
-			printf("\n");
-		else
-			printf(" ");
-	}
+if (INDEX == EI_NIDENT - 1)
+printf("\n");
+else
+printf(" ");
+}
 }
 
 /**
@@ -65,22 +65,22 @@ void print_magic(unsigned char *e_ident)
 */
 void print_class(unsigned char *e_ident)
 {
-	printf(" Class: ");
+printf(" Class: ");
 
-	switch (e_ident[EI_CLASS])
-	{
-		case ELFCLASSNONE:
-			printf("none\n");
-			break;
-		case ELFCLASS32:
-			printf("ELF32\n");
-			break;
-		case ELFCLASS64:
-			printf("ELF64\n");
-			break;
-		default:
-			printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-	}
+switch (e_ident[EI_CLASS])
+{
+case ELFCLASSNONE:
+printf("none\n");
+break;
+case ELFCLASS32:
+printf("ELF32\n");
+break;
+case ELFCLASS64:
+printf("ELF64\n");
+break;
+default:
+printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+}
 }
 
 /**
@@ -89,22 +89,22 @@ void print_class(unsigned char *e_ident)
 */
 void print_data(unsigned char *e_ident)
 {
-	printf(" Data: ");
+printf(" Data: ");
 
-	switch (e_ident[EI_DATA])
-	{
-		case ELFDATANONE:
-			printf("none\n");
-			break;
-		case ELFDATA2LSB:
-			printf("2's complement, little endian\n");
-			break;
-		case ELFDATA2MSB:
-			printf("2's complement, big endian\n");
-			break;
-		default:
-			printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-	}
+switch (e_ident[EI_DATA])
+{
+case ELFDATANONE:
+printf("none\n");
+break;
+case ELFDATA2LSB:
+printf("2's complement, little endian\n");
+break;
+case ELFDATA2MSB:
+printf("2's complement, big endian\n");
+break;
+default:
+printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+}
 }
 
 /**
@@ -113,18 +113,18 @@ void print_data(unsigned char *e_ident)
 */
 void print_version(unsigned char *e_ident)
 {
-	 printf(" Version: %d",
-			   e_ident[EI_VERSION]);
+printf(" Version: %d",
+e_ident[EI_VERSION]);
 
-	 switch (e_ident[EI_VERSION])
-	 {
-		 case EV_CURRENT:
-			 printf(" (current)\n");
-			 break;
-		 default:
-			 printf("\n");
-			 break;
-	 }
+switch (e_ident[EI_VERSION])
+{
+case EV_CURRENT:
+printf(" (current)\n");
+break;
+default:
+printf("\n");
+break;
+}
 }
 
 /**
@@ -133,43 +133,43 @@ void print_version(unsigned char *e_ident)
 */
 void print_osabi(unsigned char *e_ident)
 {
-	printf(" OS/ABI: ");
+printf(" OS/ABI: ");
 
-	switch (e_ident[EI_OSABI])
-	{
-		case ELFOSABI_NONE:
-			printf("UNIX - System V\n");
-			break;
-		case ELFOSABI_HPUX:
-			printf("UNIX - HP-UX\n");
-			break;
-		case ELFOSABI_NETBSD:
-			printf("UNIX - NetBSD\n");
-			break;
-		case ELFOSABI_LINUX:
-			printf("UNIX - Linux\n");
-			break;
-		case ELFOSABI_SOLARIS:
-			printf("UNIX - Solaris\n");
-			break;
-		case ELFOSABI_IRIX:
-			printf("UNIX - IRIX\n");
-			break;
-		case ELFOSABI_FREEBSD:
-			printf("UNIX - FreeBSD\n");
-			break;
-		case ELFOSABI_TRU64:
-			printf("UNIX - TRU64\n");
-			break;
-		case ELFOSABI_ARM:
-			printf("ARM\n");
-			break;
-		case ELFOSABI_STANDALONE:
-			printf("Standalone App\n");
-			break;
-		default:
-			printf("<unknown: %x>\n", e_ident[EI_OSABI]);
-	}
+switch (e_ident[EI_OSABI])
+{
+case ELFOSABI_NONE:
+printf("UNIX - System V\n");
+break;
+case ELFOSABI_HPUX:
+printf("UNIX - HP-UX\n");
+break;
+case ELFOSABI_NETBSD:
+printf("UNIX - NetBSD\n");
+break;
+case ELFOSABI_LINUX:
+printf("UNIX - Linux\n");
+break;
+case ELFOSABI_SOLARIS:
+printf("UNIX - Solaris\n");
+break;
+case ELFOSABI_IRIX:
+printf("UNIX - IRIX\n");
+break;
+case ELFOSABI_FREEBSD:
+printf("UNIX - FreeBSD\n");
+break;
+case ELFOSABI_TRU64:
+printf("UNIX - TRU64\n");
+break;
+case ELFOSABI_ARM:
+printf("ARM\n");
+break;
+case ELFOSABI_STANDALONE:
+printf("Standalone App\n");
+break;
+default:
+printf("<unknown: %x>\n", e_ident[EI_OSABI]);
+}
 }
 
 /**
@@ -178,8 +178,8 @@ void print_osabi(unsigned char *e_ident)
 */
 void print_abi(unsigned char *e_ident)
 {
-	printf(" ABI Version: %d\n",
-			e_ident[EI_ABIVERSION]);
+printf(" ABI Version: %d\n",
+e_ident[EI_ABIVERSION]);
 }
 
 /**
@@ -189,31 +189,30 @@ void print_abi(unsigned char *e_ident)
 */
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
-	if (e_ident[EI_DATA] == ELFDATA2MSB)
-		e_type >>= 8;
+if (e_ident[EI_DATA] == ELFDATA2MSB)
+e_type >>= 8;
 
-	printf(" Type: ");
-
-	switch (e_type)
-	{
-		case ET_NONE:
-			printf("NONE (None)\n");
-			break;
-		case ET_REL:
-			printf("REL (Relocatable file)\n");
-			break;
-		case ET_EXEC:
-			printf("EXEC (Executable file)\n");
-			break;
-		case ET_DYN:
-			printf("DYN (Shared object file)\n");
-			break;
-		case ET_CORE:
-			printf("CORE (Core file)\n");
-			break;
-		default:
-			printf("<unknown: %x>\n", e_type);
-	}
+printf(" Type: ");
+switch (e_type)
+{
+case ET_NONE:
+printf("NONE (None)\n");
+break;
+case ET_REL:
+printf("REL (Relocatable file)\n");
+break;
+case ET_EXEC:
+printf("EXEC (Executable file)\n");
+break;
+case ET_DYN:
+printf("DYN (Shared object file)\n");
+break;
+case ET_CORE:
+printf("CORE (Core file)\n");
+break;
+default:
+printf("<unknown: %x>\n", e_type);
+}
 }
 
 /**
@@ -230,4 +229,5 @@ if (e_ident[EI_DATA] == ELFDATA2MSB)
 e_entry = ((e_entry << 8) & 0xFF00FF00) |
 ((e_entry >> 8) & 0xFF00FF);
 e_entry = (e_entry << 16) | (e_entry >> 16);
+}
 }
