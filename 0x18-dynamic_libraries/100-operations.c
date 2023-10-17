83bcf7de731f5d22022b4616f2e3cc7278d73bb1
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2023-10-17 01:42:11 +0000

    update

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+* add - ADDITION
+* @a: first int
+* @b: second int
+* Return: SUM
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - SUBTRACTION
+* @a: first int
+* @b: second int
+* Return: DIFFERENCE
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - MULTIPLICATION
+* @a: first int
+* @b: second int
+* Return: PRODUCT
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - DIVISON
+* @a: first int
+* @b: second int
+* Return: DIVISION OF NUMBERS.
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - get remainder of the division of 2 numbers
+* @a: first int
+* @b: second int
+* Return: remainder of division of the numbers
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
